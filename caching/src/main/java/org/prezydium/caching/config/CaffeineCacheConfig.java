495d17dda95cf45d7f8fded1ce430ae3ed2e3ba4
krzysztof.syska <prezydium834@gmail.com>
2018-09-18 22:55:22 +0200

    Added Caffeine cache

---
+++ caching/src/main/java/org/prezydium/caching/config/CaffeineCacheConfig.java
@@ -0,0 +1,31 @@
+package org.prezydium.caching.config;
+
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.boot.autoconfigure.cache.CacheProperties;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+@Profile("caffeine")
+@Configuration
+@EnableCaching
+public class CaffeineCacheConfig {
+
+    @Bean
+    public CacheManager cacheManager() {
+
+        CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
+        Caffeine caffeine = Caffeine.newBuilder().expireAfterWrite(5, TimeUnit.SECONDS);
+        caffeineCacheManager.setCaffeine(caffeine);
+        return caffeineCacheManager;
+    }
+}
