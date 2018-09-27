60f1e3b53dbf40809a4932c46c813f1f21c2da73
kristoff <prezydium834@gmail.com>
2018-09-27 17:48:13 +0200

    Import cleanup

--- caching/src/main/java/org/prezydium/caching/config/CaffeineCacheConfig.java
+++ caching/src/main/java/org/prezydium/caching/config/CaffeineCacheConfig.java
@@ -2,17 +2,13 @@ package org.prezydium.caching.config;
 
 
 import com.github.benmanes.caffeine.cache.Caffeine;
-import org.springframework.boot.autoconfigure.cache.CacheProperties;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
-import org.springframework.cache.concurrent.ConcurrentMapCache;
-import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 @Profile("caffeine")
@@ -24,7 +20,7 @@ public class CaffeineCacheConfig {
     public CacheManager cacheManager() {
         CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
         Caffeine caffeine = Caffeine.newBuilder()
-                .expireAfterWrite(5, TimeUnit.SECONDS)
+                .expireAfterWrite(30, TimeUnit.SECONDS)
                 .maximumSize(10);
         caffeineCacheManager.setCaffeine(caffeine);
         return caffeineCacheManager;
--- caching/src/main/java/org/prezydium/caching/controller/DevViewController.java
+++ caching/src/main/java/org/prezydium/caching/controller/DevViewController.java
@@ -6,7 +6,6 @@ import org.prezydium.caching.model.Gender;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Arrays;
